0ac2e39d5be9be9360ad17a3826a5d5ba3b5e0eb
Asahel Enock Kigen <kirwaasahel@gmail.com>
2023-05-19 18:26:37 +0300

    [UPDATE]: Env Update

--- next.config.js
+++ next.config.js
@@ -1,12 +1,12 @@
 /** @type {import('next').NextConfig} */
-const webpack = require('webpack');
-const { parsed: myEnv } = require('dotenv').config();
+// const webpack = require('webpack');
+// const { parsed: myEnv } = require('dotenv').config();
 
 const nextConfig = {
-    webpack(config) {
-        config.plugins.push(new webpack.EnvironmentPlugin(myEnv));
-        return config;
-    },
+    // webpack(config) {
+    //     config.plugins.push(new webpack.EnvironmentPlugin(myEnv));
+    //     return config;
+    // },
 }
 
 module.exports = nextConfig
